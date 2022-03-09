36407df806cc1c4409978ca478c7bb212f7c56ea
abdulloh99 <abdullox19990604@gmail.com>
2022-03-09 17:04:00 +0500

    o'zgarish

--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class CompanyInfo(models.Model):
     Company_name = models.CharField(max_length=15,blank=True)
+    Company_phone = models.CharField(max_length=15,)
+    Company_secondPhone = models.CharField(max_length=15,blank=True)
     Logo = models.ImageField("LOGO : 178x117 o'lcham tavsiya etiladi",upload_to="logo")
     info = models.TextField("Konpaniya haqida ma'lumot",)
 
