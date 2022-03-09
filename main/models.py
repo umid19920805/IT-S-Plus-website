24b5d4a96f408a6f4fc4788eca23825734bffb77
abdulloh99 <abdullox19990604@gmail.com>
2022-03-09 15:20:21 +0500

    Model bn admin o'zgardi

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class CompanyInfo(models.Model):
+    Company_name = models.CharField(max_length=15,blank=True)
+    Logo = models.ImageField("LOGO : 178x117 o'lcham tavsiya etiladi",upload_to="logo")
+    info = models.TextField("Konpaniya haqida ma'lumot",)
+
+
+    def __str__(self):
+      return self.Company_name
+    class Meta:
+        verbose_name_plural = "Konpaniya ma'lumotlari va u haqida"
